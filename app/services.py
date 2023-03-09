5598949af1c86ed6f92b3bce3c1ea2df83619250
Christopher Diaz <dayandnightchrisd@gmail.com>
2023-03-09 10:45:12 -0500

    update, delete country services added.

--- app/services.py
+++ app/services.py
@@ -22,9 +22,42 @@ def create_country(data):
     mycursor.close()
     conn.myconn.close()
 
+#Update a country record by its ID
+def update_country(country_id, data):
+    #open connection
+    conn.myconn._open_connection()
+    mycursor = conn.myconn.cursor()
 
 
-#Gets all records from country table using SQL
+    name = data['Name']
+    population = data['Population']
+    continent = data['Continent']
+
+    #Execute the SQL
+    mysql = "UPDATE Country SET Name = %s, Population = %s, Continent = %s WHERE CountryId = %s"
+    values = (name, population, continent, country_id)
+    mycursor.execute(mysql, values)
+
+    #Close connection
+    mycursor.close()
+    conn.myconn.close()
+
+#Delete a country record by its ID
+def delete_country(country_id):
+    #open connection
+    conn.myconn._open_connection()
+    mycursor = conn.myconn.cursor()
+
+    #Execute the SQL
+    mysql = "DELETE FROM Country WHERE CountryId = %s"
+    value = (country_id,)
+    mycursor.execute(mysql, value)
+
+    #Close connection
+    mycursor.close()
+    conn.myconn.close()
+
+#Read all records from country table using SQL
 def all_countries():
     #open connection
     conn.myconn._open_connection()
